7f1ad425b0889b7c7bfac49d3bebbf2490874b22
Gerard Loriz <gerardloriz@ginebro.cat>
2024-03-07 01:39:21 +0100

    new org

--- Python/sniffing_scapy.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import *
-   
-   def packet_callback(packet):
-       if packet.haslayer(HTTPRequest):
-           print(packet.show())
-   
-   sniff(prn=packet_callback, store=0)
--- Python/tracker.py
+++
@@ -1,8 +0,0 @@
-import phonenumbers
-from phonenumbers import geocoder, carrier, timezone
-number = input("Enter your phone: ")
-phone_number = phonenumbers.parse(number)
-print (f"Location: {geocoder.description_for_number(phone_number, 'en')}")
-print (f"Carrier: {carrier.name_for_number(phone_number, 'en')}")
-print (f"Time Zone: {timezone.time_zones_for_number(phone_number)}")
-
